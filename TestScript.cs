b68924037ff6657f93bb902020d387f00522499c
DESKTOP-2CAFQ34\Admin <yuriibenko5@gmail.com>
2024-11-09 17:52:39 +0200

    TestScript
    TestScript

---
+++ TestScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public Transform transformCube;
+    public Transform transformSphere;
+    public float x;
+    public int speed;
+    private void Update()
+    {
+        x += speed * Time.deltaTime;
+      transformCube.position = new Vector3(x,2f,25f);
+    }
+}

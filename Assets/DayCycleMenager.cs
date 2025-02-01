7d8804d7b452bbdd453a70d68149e51404287fed
Danylo-filipovych <danylofilipovych2010@gmail.com>
2025-02-01 13:47:34 +0200

    map

--- Assets/DayCycleMenager.cs
+++ Assets/DayCycleMenager.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DayCycleMenager : MonoBehaviour
+
+
 {
+    [Range(0f, 1f)]
+    public float TimeOfDay;
+    public float DayDuration = 30f;
+
+    public Light sun;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,8 @@ public class DayCycleMenager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TimeOfDay += Time.deltaTime / DayDuration;
+        if(TimeOfDay >= 1) TimeOfDay -= 1;
+        sun.transform.localRotation = Quaternion.Euler(TimeOfDay * 360f, 180, 0);
     }
 }
